ab3deadcb0251a47fd3a068f9e1b2b9422e2d0da
rajputfc <95620124+himashubhi24@users.noreply.github.com>
2025-02-12 16:16:04 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -156,7 +156,7 @@ async def not_joined(client: Client, message: Message):
           ),
             InlineKeyboardButton(
                 "Join Channel",
-                url="https://okbra.net/tg?start=geZAec58"
+                url="https://t.me/Aifaceswapbot_bot?start=PpAj2vFD"
             )
         ]
     ]
