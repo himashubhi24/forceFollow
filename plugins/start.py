cb200ea7750018d6a40739ecab84ef0f364ab832
rajputfc <95620124+himashubhi24@users.noreply.github.com>
2025-03-25 16:36:52 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -156,7 +156,7 @@ async def not_joined(client: Client, message: Message):
           ),
             InlineKeyboardButton(
                 "Join Channel",
-                url="https://youtube.com/@alishacrickett?si=m1OjBSalrOqn9C9c"
+                url="https://t.me/+6G_DPMVfOwQ2NjQ1"
             )
         ]
     ]
