e1f9ce36310bfe1d7626c65c35c7eb62bf14e144
rajputfc <95620124+himashubhi24@users.noreply.github.com>
2025-04-22 01:46:43 +0530

    Update start.py

--- plugins/start.py
+++ plugins/start.py
@@ -156,7 +156,7 @@ async def not_joined(client: Client, message: Message):
           ),
             InlineKeyboardButton(
                 "Join Channel",
-                url="https://t.me/+6G_DPMVfOwQ2NjQ1"
+                url="https://t.me/+8QXycynnk0o5NjU0"
             )
         ]
     ]
